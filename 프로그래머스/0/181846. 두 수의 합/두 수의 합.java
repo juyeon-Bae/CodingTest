50d00e0dc894a82a24750799698b31966b946f06
배주연 <99193199+juyeon-Bae@users.noreply.github.com>
2024-01-28 00:59:07 +0900

    [level 0] Title: 두 수의 합, Time: 228.68 ms, Memory: 98.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181846. 두 수의 합/두 수의 합.java
@@ -0,0 +1,11 @@
+import java.math.BigInteger;
+class Solution {
+    public String solution(String a, String b) {
+       
+          BigInteger num1 = new BigInteger(a);
+          BigInteger num2 = new BigInteger(b);
+          BigInteger sum = num1.add(num2);
+          String result = String.valueOf(sum);
+          return result;
+    }
+}
