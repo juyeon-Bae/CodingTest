d3a38e47a8dcf66c3163316f33f715e27f744059
배주연 <99193199+juyeon-Bae@users.noreply.github.com>
2024-02-07 00:46:28 +0900

    [level 0] Title: 각도기, Time: 0.03 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/0/120829. 각도기/각도기.java
+++ 프로그래머스/0/120829. 각도기/각도기.java
@@ -1,18 +1,7 @@
 class Solution {
     public int solution(int angle) {
-        int answer = 0;
-        if(angle<90){
-            answer =1;
-        }
-        else if(angle==90){
-            answer =2;
-        }
-        else if(angle<180){
-            answer =3;
-        }
-        else if(angle ==180){
-            answer = 4;
-        }
-        return answer;
+       return angle==180 ? 4 : angle>90 ? 3 : angle == 90 ?2 : angle<90 ?  1:0;
+       
+         
     }
 }
