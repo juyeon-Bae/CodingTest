812941ef7e8cce9d1415403557ee7723be0f06d4
배주연 <99193199+juyeon-Bae@users.noreply.github.com>
2024-02-19 17:01:36 +0900

    [level 0] Title: 첫 번째로 나오는 음수, Time: 0.02 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/0/181896. 첫 번째로 나오는 음수/README.md
+++ 프로그래머스/0/181896. 첫 번째로 나오는 음수/README.md
@@ -1,10 +1,10 @@
 # [level 0] 첫 번째로 나오는 음수 - 181896 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181896?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181896) 
 
 ### 성능 요약
 
-메모리: 84.5 MB, 시간: 0.02 ms
+메모리: 76.4 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 2월 0일 15:57:7
+2024년 2월 1일 17:1:33
 
 ### 문제 설명
 
--- 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.java
+++ 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.java
@@ -1,16 +1,15 @@
- 
 class Solution {
     public int solution(int[] num_list) {
-        int answer =0;
+        int answer = 0;
         for(int i=0; i<num_list.length; i++){
-            if(num_list[i]<0){
-                answer = i;
-                break;
-            }
+           if(num_list[i]<0){
+               answer = i;
+               break;
+           }
             else{
-                answer = -1;
+                answer =-1;
             }
         }
-       return  answer;
+        return answer;
     }
 }
