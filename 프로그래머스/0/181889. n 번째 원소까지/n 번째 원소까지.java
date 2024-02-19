bbf83ed3a098894ce8ec3ed8debe988d5be07236
배주연 <99193199+juyeon-Bae@users.noreply.github.com>
2024-02-20 00:15:49 +0900

    [level 0] Title: n 번째 원소까지, Time: 0.02 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/0/181889. n 번째 원소까지/n 번째 원소까지.java
+++ 프로그래머스/0/181889. n 번째 원소까지/n 번째 원소까지.java
@@ -1,10 +1,6 @@
+import java.util.Arrays;
 class Solution {
     public int[] solution(int[] num_list, int n) {
-        int[] answer = new int[n]; //배열 크기 생성
-  
-        for(int i=0; i<n; i++){
-            answer[i] = num_list[i];
-        }
-        return answer;
+         return Arrays.copyOfRange(num_list,0,n);
     }
 }
