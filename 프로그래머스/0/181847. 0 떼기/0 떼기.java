47af654f5d85f77cfec17b3bb6ec790fa8a17157
배주연 <99193199+juyeon-Bae@users.noreply.github.com>
2024-02-22 21:03:00 +0900

    [level 0] Title: 0 떼기, Time: 0.02 ms, Memory: 73.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String solution(String n_str) {
+         
+        int reset = Integer.parseInt(n_str);
+        String answer =String.valueOf(reset);
+        return answer;
+    }
+}
