1e8ae0de6f49f6401cc3fb67fac973ba3a4b26db
배주연 <99193199+juyeon-Bae@users.noreply.github.com>
2024-05-07 23:44:40 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
@@ -1,8 +1,3 @@
 function solution(s) {
-    var answer = '';
-    for(x of s){
-        if(s.length%2==1) answer = s.charAt(s.length/2);
-        else answer = s.charAt(s.length/2-1) + s.charAt(s.length/2); 
-    }
-    return answer;
+    return s.length%2==1 ? s.substr(s.length/2,1) : s.substr(s.length/2-1,2);
 }
