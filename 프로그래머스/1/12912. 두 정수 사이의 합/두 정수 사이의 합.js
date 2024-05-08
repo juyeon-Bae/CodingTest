2635d5e2da9276aed388322cb9ed904989b6f674
배주연 <99193199+juyeon-Bae@users.noreply.github.com>
2024-05-09 00:58:05 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.js
+++ 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.js
@@ -1,6 +1,12 @@
 function solution(a, b) {
+    /*  방법1
     var answer = 0;
-    for(var i=a; i<=b; i++) answer += i;
-    if(a>b) for(var i=a; i>=b; i--) answer +=i;
-    return answer;
+    var start = Math.min(a,b); 
+    var end = Math.max(a,b);
+    
+    for(var i=start; i<=end; i++) answer +=i; 
+    */
+    
+    // 방법2 (가우스의 합계 공식)
+    return (a+b) * (Math.abs(b-a)+1)/2;
 }
