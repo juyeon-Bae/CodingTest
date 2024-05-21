7d564d8993f082305a6d07f5e7e1e91f9bfea7d5
배주연 <99193199+juyeon-Bae@users.noreply.github.com>
2024-05-21 16:36:41 +0900

    [level 0] Title: ad 제거하기, Time: 0.10 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/181870. ad 제거하기/ad 제거하기.js
+++ 프로그래머스/0/181870. ad 제거하기/ad 제거하기.js
@@ -1,6 +1,3 @@
 function solution(strArr) {
-    var answer = [];
-    for(x of strArr)
-    if(!x.includes("ad")) answer.push(x)
-    return answer;
+    return strArr.filter((x)=>!x.includes('ad'));
 }
