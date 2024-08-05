f35c5d52fecc56af02bbf2bc9937d92c8fa598ab
배주연 <99193199+juyeon-Bae@users.noreply.github.com>
2024-08-05 20:49:57 +0900

    [level 1] Title: 음양 더하기, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
@@ -1,15 +1,7 @@
 function solution(absolutes, signs) {
-    var answer = 123456789;
-    var sum = 0
-    for(let i = 0 ; i<signs.length; i++){
-        if(signs[i] === true){
-            absolutes[i] +=0
-        }
-        else{
-            absolutes[i] = absolutes[i] - (absolutes[i] *2)
-        }
-        sum +=absolutes[i]
-       
+    var answer = 0;
+    for(let i=0; i<signs.length; i++){
+        signs[i] == true ? answer+=absolutes[i] : answer -=absolutes[i]
     }
-    return sum;
+    return answer;
 }
