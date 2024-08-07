b3be7689a0f3c35d153f0edbd16c3d9fb64a94a2
배주연 <99193199+juyeon-Bae@users.noreply.github.com>
2024-08-07 23:48:44 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 46.59 ms, Memory: 83.2 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,8 +1,4 @@
 function solution(arr)
 {
-    var answer = [];
-    for(let i=0; i<arr.length; i++){
-        if(arr[i] !== arr[i+1]) answer.push(arr[i])
-    }
-    return answer;
+    return arr.filter((x,idx) => x  != arr[idx+1]);
 }
