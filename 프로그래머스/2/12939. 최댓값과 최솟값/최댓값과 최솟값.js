c1d9ab5a367dfe4a6d0587a6063fb62fce95d432
배주연 <99193199+juyeon-Bae@users.noreply.github.com>
2024-08-10 11:50:11 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.09 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -1,8 +1,4 @@
 function solution(s) {
-   var answer = '';
-   let arr = s.split(' ').map(Number)
-   arr.sort((a,b) => a-b)
-   answer += arr[0] + ' '
-   answer += arr[arr.length-1]
-   return answer;
+    let arr = s.split(' ')
+    return Math.min(...arr) + ' '+ Math.max(...arr);
 }
