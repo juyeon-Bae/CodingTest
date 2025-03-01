6e7a52c5faa3ec40619562cb3ee753c9ca592812
배주연 <99193199+juyeon-Bae@users.noreply.github.com>
2025-03-02 05:58:09 +0900

    [level 1] Title: 과일 장수, Time: 214.24 ms, Memory: 83.3 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.js
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.js
@@ -1,24 +1,9 @@
 function solution(k, m, score) {
     var answer = 0;
-     let arr = []
-     
-    score.sort((a,b)=> b - a)
-      
+    score.sort((a,b) => b - a)
     for(let i=0; i<score.length; i +=m){
-         let group = score.slice(i,i+m)
-          if(group.length === m) {
-              arr.push(group)
-          }
+        let min = score[i + m -1]
+       if(min) answer += min * m
     }
-    console.log(arr)
-    
-    answer = arr.reduce((acc, cur)=>{
-        let min = Math.min(...cur)
-        return acc + min * m
-    },0)
-    return answer
+    return answer;
 }
-
-  
- 
- 
